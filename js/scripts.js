81c3050d79521a2f90bf3012715ba19ca399c46d
Maureen <mstienei@gmail.com>
2020-06-10 18:30:39 +0300

    update js values

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,28 @@ $(document).ready(function(){
   $("#col1rw4").click(function(){
     $(this).text($(this).text() == 'Variable Naming Conventions' ? 'JavaScript guidelines rules for variable naming and declaration.' : 'Variable Naming Conventions');
   });
+  $("#col2rw1").click(function(){
+    $(this).text($(this).text() == 'Functions' ? 'A block of code designed to perform a particular task' : 'Functions');
+  });
+  $("#col2rw2").click(function(){
+    $(this).text($(this).text() == 'Strings' ? 'A JavaScript string stores a series of characters like' : 'Strings');
+  });
+  $("#col2rw3").click(function(){
+    $(this).text($(this).text() == 'Methods' ? 'JavaScript methods are actions that can be performed on objects.' : 'Methods');
+  });
+  $("#col2rw4").click(function(){
+    $(this).text($(this).text() == 'Arguments' ? 'Values passed to the function when it is invoked.' : 'Arguments');
+  });
+  $("#col3rw1").click(function(){
+    $(this).text($(this).text() == 'Parameters' ? 'Variables listed as a part of the function definition.' : 'Parameters');
+  });
+  $("#col3rw2").click(function(){
+    $(this).text($(this).text() == 'Return' ? 'Return statement stops the execution of a function and returns a value from that function.' : 'Return');
+  });
+  $("#col3rw3").click(function(){
+    $(this).text($(this).text() == 'Chaining methods' ? 'Repeatedly calling one method after another on an object, in one continuous line of code.' : 'Chaining methods');
+  });
+  $("#col3rw4").click(function(){
+    $(this).text($(this).text() == 'Booleans' ? 'Boolean data type. It can only take the values true or false.' : 'Booleans');
+  });
 })

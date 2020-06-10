c81801621fe5ca47205b8720fe4717bb0537a61b
Maureen <mstienei@gmail.com>
2020-06-10 16:35:57 +0300

    add jquery text method to toggle text inside one element

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#col1rw1").click(function(){
+    $(this).text($(this).text() == 'Javascript' ? 'JavaScript is the programming language of HTML and the Web' : 'Javascript');
+  });
+})

1f3bbe7f7edf8936fd3088ad90f94c77412d301a
Maureen <mstienei@gmail.com>
2020-06-10 17:53:41 +0300

    add id to the p tags

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
   $("#col1rw1").click(function(){
-    $(this).text($(this).text() == 'Javascript' ? 'JavaScript is the programming language of HTML and the Web' : 'Javascript');
+    $(this).text($(this).text() == 'Javascript' ? 'Javascript is the programming language of HTML and the Web.' : 'Javascript');
+  });
+  $("#col1rw2").click(function(){
+    $(this).text($(this).text() == 'Operators' ? 'JavaScript operators are used to assign values, compare values, perform arithmetic operations, and more.' : 'Operators');
+  });
+  $("#col1rw3").click(function(){
+    $(this).text($(this).text() == 'Variables' ? 'Variables are containers for storing data values.' : 'Variables');
+  });
+  $("#col1rw4").click(function(){
+    $(this).text($(this).text() == 'Variable Naming Conventions' ? 'JavaScript guidelines rules for variable naming and declaration.' : 'Variable Naming Conventions');
   });
 })
